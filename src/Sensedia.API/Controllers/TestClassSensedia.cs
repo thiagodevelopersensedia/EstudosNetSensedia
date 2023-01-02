eb964f580e4cf988e9cb39de3f579c9564946fcb
Thiago Almeida <thiago.almeida@sensedia.com>
2023-01-02 08:49:51 -0300

    Add project files.

---
+++ src/Sensedia.API/Controllers/TestClassSensedia.cs
@@ -0,0 +1,8 @@
+﻿namespace Sensedia.API.Controllers
+{
+    public class TestClassSensedia
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ src/Sensedia.API/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigins", p =>
+    {
+        p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();project\test\angular\estudo
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowOrigins");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
