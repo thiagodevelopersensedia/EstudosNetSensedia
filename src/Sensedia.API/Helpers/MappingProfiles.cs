c23c5ed6a1c4b56d7656622571ee8f6b3b60ba22
Thiago Almeida <thiago.sensedia@sorteonline.com.br>
2023-03-06 09:19:50 -0300

    feat: add automapper to ProductDTO

---
+++ src/Sensedia.API/Helpers/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Sensedia.Core.DTO;
+using Sensedia.Core.Entities;
+
+namespace Sensedia.API.Helpers
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Product, ProductToReturnDTO>()
+                .ForMember(p => p.ProductName, o => o.MapFrom(x => x.Name))
+                .ForMember(p => p.ProductPrice, o => o.MapFrom(x => x.Price))
+                .ForMember(p => p.ProducDescription, o => o.MapFrom(x => x.Description))
+                .ForMember(p => p.ProductBrand, o => o.MapFrom(x => x.ProductBrand.Name))
+                .ForMember(p => p.ProductType, o => o.MapFrom(x => x.ProductType.Name));
+        }
+
+    }
+}
