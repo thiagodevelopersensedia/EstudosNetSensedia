bcad72138cbb3fcbc2eed36034907c8b2257df9d
Thiago Almeida <thiago.sensedia@sorteonline.com.br>
2023-04-10 09:20:34 -0300

    fix cleaning up startup

---
+++ src/Sensedia.API/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sensedia.API.Error;
+using Sensedia.Core.Interfaces.Generic;
+using Sensedia.Core.Interfaces.Products;
+using Sensedia.Infrastructure.Context;
+using Sensedia.Infrastructure.Repository.Products;
+
+namespace Sensedia.API.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+            IConfigurationRoot config)
+        {
+
+            // Add services to the container.
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddControllers();
+            var connectionString = config.GetConnectionString("SensediaLocalSqlLite");
+
+            services.AddDbContext<SensediaContext>(x => x.UseSqlite(connectionString));
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigins", p =>
+                {
+                    p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+                });
+            });
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.Configure<ApiBehaviorOptions>(options =>
+            {
+                options.InvalidModelStateResponseFactory = actionContext =>
+                {
+                    var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .SelectMany(x => x.Value.Errors)
+                    .Select(x => x.ErrorMessage).ToArray();
+
+                    var errorResponse = new ApiValidationErroResponse
+                    {
+                        Errors = errors
+                    };
+                    return new BadRequestObjectResult(errorResponse);
+                };
+            });
+
+            return services;
+        }
+    }
+}
