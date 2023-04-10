45cfae2ca033779fee358bdda7a596a62c484dd7
Thiago Almeida <thiago.sensedia@sorteonline.com.br>
2023-04-10 09:20:34 -0300

    fix cleaning up startup

--- src/Sensedia.API/Program.cs
+++ src/Sensedia.API/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sensedia.API.Error;
+using Sensedia.API.Extensions;
 using Sensedia.API.Middleware;
 using Sensedia.Core.Entities;
 using Sensedia.Core.Interfaces.Generic;
@@ -24,45 +25,7 @@ var configManager = new ConfigurationBuilder()
     .AddEnvironmentVariables()
     .Build();
 
-// Add services to the container.
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("SensediaLocalSqlLite");
-
-builder.Services.AddDbContext<SensediaContext>(x => x.UseSqlite(connectionString));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowOrigins", p =>
-    {
-        p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-    });
-});
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-        .Where(e => e.Value.Errors.Count > 0)
-        .SelectMany(x => x.Value.Errors)
-        .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse = new ApiValidationErroResponse
-        {
-            Errors = errors
-        };
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
-
+builder.Services.AddApplicationServices(configManager);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
